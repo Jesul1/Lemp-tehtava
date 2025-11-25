2d9bf351abd94d37fe8774991f66cc2fc0118876
Jesul1 <omajesse.vaarala@gmail.com>
2025-11-25 22:11:10 +0200

    Api tehtävä

---
+++ fetch_weather.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import requests
+import os
+import mysql.connector
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+print("Loaded keys:", list(os.environ.keys()))
+
+API_KEY = os.environ['api_key']
+CITY = 'Oulu'
+URL =f'https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric'
+
+conn = mysql.connector.connect(host=os.environ['db_host'], user=os.environ['db_user'],
+password=os.environ['db_pass'], database=os.environ['db_name'])
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS weather_data (id INT AUTO_INCREMENT PRIMARY KEY, city VARCHAR(50), temperature FLOAT, description VARCHAR(100), timestamp DATETIME)''')
+response = requests.get(URL)
+data = response.json()
+temp = data['main']['temp']
+desc = data['weather'][0]['description']
+timestamp = datetime.now()
+cursor.execute('INSERT INTO weather_data (city, temperature, description, timestamp) VALUES (%s, %s, %s, %s)', (CITY, temp, desc, timestamp))
+conn.commit()
+cursor.close()
+conn.close()
+print(f'Data tallennettu: {CITY} {temp}°C {desc}')
--- streamlit-app/streamlit.py
+++ streamlit-app/streamlit.py
@@ -1,7 +1,9 @@
+import os
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
+import mysql.connector
+import requests
 
 @st.cache_resource
 def mySql():
@@ -20,6 +22,30 @@ def main():
     df2 = pd.DataFrame(data, columns=["date","temperature"])
     totalKg = px.line(df2, x="date", y="temperature")
     st.plotly_chart(totalKg, use_container_width=True)
+
+    conn = st.connection('mysql', type='sql')
+    df = conn.query(
+        "SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50",
+        ttl="0"
+    )
+    st.title('Säädata Oulusta')
+    st.dataframe(df)
+
+    def display_cat():
+        url = 'https://cataas.com/cat?json=true'
+
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            image_url = data['url']
+            st.image(image_url, caption='Cat Image', use_container_width=True)
+            # Process the response data as needed
+        else:
+            print('Request failed with status code:', response.status_code)
+
+    if st.button("Get cat image"):
+        display_cat()
 
 if __name__ == "__main__":
     main()
